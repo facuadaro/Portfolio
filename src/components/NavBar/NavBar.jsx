619f0f6669c6c6613900c6e6defd8c308a959e05
Facundo Adaro <adarbox@gmail.com>
2023-04-20 21:26:15 -0300

    first commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import * as S from './NavBar.styles'
+
+
+function NavBar() {
+  return (
+    <S.ContainerNavbar>
+        Container navbar
+    </S.ContainerNavbar>
+  )
+}
+
+export default NavBar
