b281430b647951817f2b69f04ff3746a0ee755d6
Facundo Adaro <adarbox@gmail.com>
2023-04-20 23:14:09 -0300

    feat: add burger menu mobile and desktop

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from './NavBar.styles'
 
 
 function NavBar() {
+
+    const [showMenuBurger, setShowMenuBurger] = useState(false)
+
+    console.log(showMenuBurger)
+
   return (
     <S.ContainerNavbar>
-        Container navbar
+        <S.ContainerLogo>
+            <S.ImageLogo src="/img/aguila.png" alt="Logo Facundo Adaro" />
+            <div>
+                <h1>Facundo Adaro</h1>
+                <h2>Frontend Developer</h2>
+            </div>
+        </S.ContainerLogo>
+        <S.ContainerBurger onClick={() => setShowMenuBurger(!showMenuBurger)}>
+            <S.BurgerMenu></S.BurgerMenu>
+            <S.BurgerMenu></S.BurgerMenu>
+            <S.BurgerMenu></S.BurgerMenu>
+        </S.ContainerBurger>
+        <S.ContainerItems showMenuBurger={showMenuBurger}>
+            <p>About Me</p>
+            <p>Projects</p>
+            <S.Button>🤙🏼Contact Me</S.Button>
+        </S.ContainerItems>
     </S.ContainerNavbar>
   )
 }
